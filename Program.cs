bb00d563ace8943b2d8e9b3114a4af5adb83adca
ahmedms2104 <ahmedms2104@gmail.com>
2024-11-25 12:44:45 +0300

    1,2,5

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Globalization;
+using System.Security.Cryptography;
+using System.Text;
+
+int [] ac =[50,44,66,22,33];
+ J;Console.WriteLine();
+for(int i =0 ; i<ac.Length;i++){
+   Console.WriteLine(ac[i]);
+   
+   int sum =+ac[i] ;
+
+   Console.WriteLine(sum);
+}
+
+class Student{
+
+    
+    public void  Main(String[]arg){
+        string name;
+        int Age;
+     string Grade;
+        string student ;
+        Console.WriteLine(" , Name : {$name}, Age : {$Age},Grade : {$Graderade}");
+
+        student("Ahmed" , 24 , 5);
+
+
+
+    }
+
+}
+string day  ;
+Console.ReadLine();
+switch(day)
+   case (day =="1"){
+        Console.WriteLine("starday");
+
+
+    }
+    break;
+    case (day =="2"){
+        Console.WriteLine("Sunday");
+
+
+    }
+    break;
+
+    case (day =="3"){
+        Console.WriteLine("monday");
+
+
+    }
+    break;
+
+    case(day == "4"){
+        Console.WriteLine("Tuesday");
+    }
+    break ;
+
+     case(day == "5"){
+        Console.WriteLine("");
+   
+    break ;
+
+}
+
+int num ; 
+
+Console.ReadLine();
+
+
